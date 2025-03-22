cb977181c680dbc6d4cb6da38022b02bde75706f
LeartaPllana <leartapllana9@gmail.com>
2025-03-22 03:29:56 +0100

    Created project with VUE JS

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const message = ref('Learta')
+
+setTimeout(() => {
+  message.value = 'Perfundoj kursi per sot!'
+}, 5000)
+</script>
+
+<style>
+p {
+  color: red;
+}
+</style>
