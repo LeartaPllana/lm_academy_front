fee7777988543e6c21a5a8adb043bbc5714e81a6
LeartaPllana <leartapllana9@gmail.com>
2025-04-10 00:25:13 +0200

    Pushing the default layout

---
+++ src/components/default/general/HeaderDefault.vue
@@ -0,0 +1,23 @@
+<template>
+      <!-- header -->
+      <header>
+      <div class="flex justify-between items-center border-b-textPrimary border-b-4 py-4">
+        <h1 class="logo text-5xl text-textPrimary">LM Academy</h1>
+        <nav class="text-3xl flex gap-2">
+          <router-link :to="{ name: 'home' }" active-class="border-b-textPrimary border-b-2"
+            >Home</router-link
+          >
+          <span>|</span>
+          <router-link to="/about-us" active-class="border-b-textPrimary border-b-2"
+            >About Us</router-link
+          >
+          <span>|</span>
+          <router-link to="/login" active-class="border-b-textPrimary border-b-2"
+            >Login</router-link
+          >
+          <span>|</span>
+        </nav>
+      </div>
+    </header>
+    <!-- end header -->
+</template>
---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,20 @@
+<script setup>
+import HeaderDefault from '@/components/default/general/HeaderDefault.vue'
+import FooterDefault from '@/components/default/general/FooterDefault.vue'
+</script>
+
+<template>
+  <main class="px-4">
+    <!-- header -->
+    <HeaderDefault />
+    <!-- end header -->
+    <!-- content -->
+    <div class="wrapper min-h-[80vh] ">
+      <router-view />
+    </div>
+    <!-- end content -->
+    <!-- footer -->
+    <FooterDefault />
+    <!-- end footer -->
+  </main>
+</template>
